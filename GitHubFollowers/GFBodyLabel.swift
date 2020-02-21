ab35f87ce43045f559bd1e46c029feb9ce91ce62
pitt500 <pedro.rojaslo50@gmail.com>
2020-02-21 05:36:42 -0700

    Add custom labels

---
+++ GitHubFollowers/GFBodyLabel.swift
@@ -0,0 +1,34 @@
+//
+//  GFBodyLabel.swift
+//  GitHubFollowers
+//
+//  Created by projas on 2/21/20.
+//  Copyright © 2020 pedrorojas.dev. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init(textAlignment: NSTextAlignment) {
+    super.init(frame: .zero)
+    self.textAlignment = textAlignment
+  }
+  
+  private func configure() {
+    textColor = .secondaryLabel
+    font = UIFont.preferredFont(forTextStyle: .body)
+    adjustsFontSizeToFitWidth = true
+    minimumScaleFactor = 0.75
+    lineBreakMode = .byWordWrapping
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+}

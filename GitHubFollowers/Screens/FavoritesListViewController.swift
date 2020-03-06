a36bd5b7601507dcc10e57c6c9ee1af3d62637ff
pitt500 <pedro.rojaslo50@gmail.com>
2020-03-06 05:18:45 -0700

    Save favorites in user defaults

--- GitHubFollowers/Screens/FavoritesListViewController.swift
+++ GitHubFollowers/Screens/FavoritesListViewController.swift
@@ -13,6 +13,15 @@ class FavoritesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      view.backgroundColor = .systemRed
+      view.backgroundColor = .systemBackground
+      
+      PersistanceManager.retrieveFavorites { result in
+        switch result {
+        case .success(let favorites):
+          print(favorites)
+        case .failure(let error):
+          print(error)
+        }
+      }
     }
 }

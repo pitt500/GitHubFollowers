c4b403e4091eed55b06129b2ce77d9b5ec182f4b
pitt500 <pedro.rojaslo50@gmail.com>
2020-03-12 05:49:07 -0700

    Refactor download image method

--- GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -11,7 +11,6 @@ import UIKit
 class GFAvatarImageView: UIImageView {
   
   let placeholderImage = UIImage(named: "avatar-placeholder")!
-  let cache = NetworkManager.shared.cache
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -27,36 +26,5 @@ class GFAvatarImageView: UIImageView {
     clipsToBounds = true
     image = placeholderImage
     translatesAutoresizingMaskIntoConstraints = false
-  }
-  
-  func downloadImage(from urlString: String) {
-    
-    let key = NSString(string: urlString)
-    if let image = cache.object(forKey: key) {
-      self.image = image
-      return
-    }
-    
-    guard let url = URL(string: urlString) else {
-      return
-    }
-    
-    let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-      guard let self = self else { return }
-      if error != nil { return }
-      
-      guard let response = response as? HTTPURLResponse,
-        response.statusCode == 200 else { return }
-      guard let data = data else { return }
-      
-      guard let image = UIImage(data: data) else { return }
-      self.cache.setObject(image, forKey: key)
-      
-      DispatchQueue.main.async {
-        self.image = image
-      }
-    }
-    
-    task.resume()
   }
 }
--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -94,4 +94,34 @@ class NetworkManager {
     
     task.resume()
   }
+  
+  func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+    let key = NSString(string: urlString)
+    if let image = cache.object(forKey: key) {
+      completed(image)
+      return
+    }
+    
+    guard let url = URL(string: urlString) else {
+      completed(nil)
+      return
+    }
+    
+    let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+      guard let self = self,
+        error == nil,
+        let response = response as? HTTPURLResponse,
+        response.statusCode == 200,
+        let data = data,
+        let image = UIImage(data: data) else {
+          completed(nil)
+          return
+      }
+      
+      self.cache.setObject(image, forKey: key)
+      completed(image)
+    }
+    
+    task.resume()
+  }
 }
